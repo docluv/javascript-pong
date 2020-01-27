0ea62bbf0cb510770222241c8e79db20b2b63d31
docluv <chris@love2dev.com>
2020-01-27 12:26:24 -0500

    a2hs

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
-Canvas Pong
+Canvas Pong Progressive Web App
 ===========
 
-This is a small experiment to implement a version of the classic Pong game in an HTML <canvas>.
+This is a small experiment to implement a version of the classic Pong game in an HTML <canvas>. It was upgraded to be a progressive web app (PWA).
+
+## The Original Version
 
  * You can find the [game here](http://codeincomplete.com/posts/2011/5/14/javascript_pong/demo.html)
  * You can find out [how it works](http://codeincomplete.com/posts/2011/5/14/javascript_pong/index.html)
 
 You can also play this game in the [Chrome Web Store](https://chrome.google.com/webstore/detail/omimkinlomnncbmnceacpkmlbfaapojj?hl=en-US)
 
+## The Progressive Web App Version
+
+The progressive web app 
+
+ * You can find the [game here](https://pong.love2dev.com/)
+ * You can find out [how it was upgraded to a Progressive Web App](https://love2dev.com/blog/pwa-pong/)
+
 
 NOTES
 =====
 
  * No support for mobile devices (need to handle touch events instead of key input)
+
+ * No responsive design
--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-const version = "1.01",
+const version = "1.02",
     preCache = "PRECACHE-" + version,
     dynamicCache = "DYNAMIC-" + version,
     preCacheFiles = [
         "/",
-        "pong.css",
+        "css/pong.css",
+        "css/bootstrap.min.css",
+        "css/animate.min.css",
+        "css/addtohomescreen.css",
         "game.js",
         "pong.js",
         "images/press1.png",
@@ -16,83 +19,83 @@ const version = "1.01",
     ];
 
 
-self.addEventListener("install", event => {
+self.addEventListener( "install", event => {
 
     self.skipWaiting();
 
-    console.log("installing precache files");
+    console.log( "installing precache files" );
 
-    caches.open(preCache).then(function (cache) {
+    caches.open( preCache ).then( cache => {
 
-        return cache.addAll(preCacheFiles);
+        return cache.addAll( preCacheFiles );
 
-    });
+    } );
 
-});
+} );
 
 
-self.addEventListener("activate", event => {
+self.addEventListener( "activate", event => {
 
     event.waitUntil(
 
-        caches.keys().then(cacheNames => {
-            cacheNames.forEach(value => {
+        caches.keys().then( cacheNames => {
+            cacheNames.forEach( value => {
 
-                if (value.indexOf(version) < 0) {
-                    caches.delete(value);
+                if ( value.indexOf( version ) < 0 ) {
+                    caches.delete( value );
                 }
 
-            });
+            } );
 
-            console.log("service worker activated");
+            console.log( "service worker activated" );
 
             return;
 
-        })
+        } )
 
     );
 
-});
+} );
 
 
-self.addEventListener("fetch", event => {
+self.addEventListener( "fetch", event => {
 
     event.respondWith(
 
-        caches.match(event.request).then(response => {
+        caches.match( event.request ).then( response => {
 
-            if (!response) {
+            if ( !response ) {
 
                 //fall back to the network fetch
-                return fetch(event.request)
-                    .then(response => {
+                return fetch( event.request )
+                    .then( response => {
 
-                        if (response) {
+                        if ( response ) {
 
-                            if (event.request.url.indexOf("manifest.json") === -1) {
+                            if ( event.request.url.indexOf( "manifest.json" ) === -1 ) {
 
                                 let copy = response.clone();
 
-                                caches.open(dynamicCache)
-                                    .then(cache => {
+                                caches.open( dynamicCache )
+                                    .then( cache => {
 
-                                        cache.put(event.request, copy);
+                                        cache.put( event.request, copy );
 
-                                    });
+                                    } );
 
                             }
 
                             return response;
                         }
 
-                    });
+                    } );
 
             }
 
             return response;
 
-        })
+        } )
 
     )
 
-});
+} );
